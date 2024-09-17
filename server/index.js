6d870cf049cecad8fa7c9372e684a73d9ce48a89
Mr-Aaryan <kc.aryan3536@gmail.com>
2024-09-17 11:39:58 +0545

    nodemailer setup

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { handleEmail } = require("./nodemailer");
+const app = express();
+
+require("dotenv").config();
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/sendEmail", handleEmail);
+
+app.listen(3000, () => {
+  console.log("server running at port 3000");
+});
